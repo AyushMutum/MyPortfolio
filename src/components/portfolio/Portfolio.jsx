319e48e5afb2bafa2d990e91cd081add1b840667
Ayush Mutum <ayushmutum6@gmail.com>
2025-09-04 16:59:09 +0530

    updated live demo

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import './portfolio.css';
 
-import IMG1 from '../../assests/animeWebsite.png';
-import IMG2 from '../../assests/todo.png'
-import IMG3 from '../../assests/chegg.png';
-import IMG4 from '../../assests/foodzone.png';
+
+import IMG2 from '../../assests/NoteApp.png'
+import IMG3 from '../../assests/fitness.png';
 import IMG5 from '../../assests/jscalculator.png';
 import IMG6 from '../../assests/movie-app.png';
-import IMG7 from '../../assests/randomquote.png';
-import IMG8 from '../../assests/country.png';
-import IMG9 from '../../assests/AgeCal.png';
 import IMG10 from '../../assests/Move.png';
+
+
+
 
 
 const data = [
   
+ 
   {
     id: 1,
     image: IMG6,
@@ -26,16 +26,16 @@ const data = [
   {
     id: 2,
     image: IMG2,
-    title: 'Todo-App',
-    github: 'https://github.com/AyushMutum',
-    demo: 'https://todo-app-two-ochre.vercel.app/'
+    title: 'NoteVerse',
+    github: 'https://github.com/AyushMutum/NoteVerse',
+    demo: 'https://noteverse-1.onrender.com/'
   },
   {
     id: 3,
     image: IMG3,
-    title: 'Chegg Clone',
+    title: 'Fitness-coach-website',
     github: 'https://github.com/AyushMutum',
-    demo: 'https://chegg-clone-xi.vercel.app/'
+    demo: 'https://a-fitness-xi.vercel.app/'
   },
   {
     id: 4,
@@ -52,42 +52,11 @@ const data = [
     github: 'https://github.com/AyushMutum',
     demo: 'https://js-calculator-fawn.vercel.app/'
   },
-  {
-    id: 6,
-    image: IMG1,
-    title: 'Anime Shopping Website ',
-    github: 'https://github.com/AyushMutum',
-    demo: 'https://anime-onlineshoping-github-io.vercel.app/'
-  },
-  {
-    id: 7,
-    image: IMG7,
-    title: 'Random Quote Generator',
-    github: 'https://github.com/AyushMutum',
-    demo: 'https://random-quote-generator-orpin.vercel.app/'
-  },
-  {
-    id: 8,
-    image: IMG8,
-    title: 'Know Countries',
-    github: 'https://github.com/AyushMutum',
-    demo: 'https://know-country-details-github-io.vercel.app/'
-  },
-  {
-    id: 9,
-    image: IMG9,
-    title: 'Age Calculator',
-    github: 'https://github.com/AyushMutum',
-    demo: 'https://age-calculator-ten-fawn.vercel.app/'
-  },
-  {
-    id: 10,
-    image: IMG4,
-    title: 'Food Zone ',
-    github: 'https://github.com/AyushMutum',
-    demo: 'https://ayushmutum.github.io/foodhub/'
-   
-  },
+
+
+
+
+
 
 
 
